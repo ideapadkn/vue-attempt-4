f4205927fa27578e22055d669ef67a65556e18ee
Kamron or Камрон <kamrush6429@gmail.com>
2023-01-11 20:48:48 +0500

    MyDialog component modal window

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="app">
-    <post-form 
+    <h1>Posts</h1>
+    <my-button
+      @click="showDialog"
+    >
+      Create
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form 
       @create="createPost"
     />
+    </my-dialog>
+    
     <post-list
-      :posts="posts"    
+      :posts="posts" 
+      @remove="removePost"
     />
   </div>
 </template>
@@ -25,12 +35,20 @@ import PostList from "@/components/PostList"
           {id: 3, title: "JavaScript 3", body: "Description"},
           {id: 4, title: "JavaScript 4", body: "Description"},
         ],
+        dialogVisible: false,
       }
     },
     methods: {
       createPost(post) {
         this.posts.push(post);
+        this.dialogVisible = false;
       },
+      removePost(post) {
+        this.posts = this.posts.filter(p => p.id !== post.id)
+      },
+      showDialog() {
+        this.dialogVisible = true;
+      }
     }, 
   }
 </script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>
+  <div 
+    v-if="posts.length > 0"
+  >
     <h3>List Users</h3>
     <post-item 
       v-for="post in posts" 
       :post="post"
+      :key="post.id"
+      @remove="$emit('remove', post)"
     />
   </div>
+  <h2
+    v-else
+    style="color: red;"
+  >
+    Post List Empty
+  </h2>
 </template>
 
 <script>

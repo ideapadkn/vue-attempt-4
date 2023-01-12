7c82ddd3e46edda0568baf806228f407fa2003a8
Kamron or Камрон <kamrush6429@gmail.com>
2023-01-12 19:13:11 +0500

    animation trasition-group

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -3,12 +3,14 @@
     v-if="posts.length > 0"
   >
     <h3>List Users</h3>
-    <post-item 
-      v-for="post in posts" 
-      :post="post"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item 
+        v-for="post in posts" 
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2
     v-else
@@ -34,5 +36,14 @@ import PostItem from '@/components/PostItem';
 </script>
 
 <style scoped>
-
+.post-list-move,
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.5s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
 </style>

2e257b9b6c0a9c7b3fc323cc247df0fd48d46317
Kamron or Камрон <kamrush6429@gmail.com>
2023-01-14 18:08:34 +0500

    vue-router base information and add navbar(event click to btn )

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
+  <navbar></navbar>
   <div class="app">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar"
   export default {
-    
-  }
+    components: {
+      Navbar,
+    }
+  } 
 </script>
 
 <style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="navbar">
+    <div 
+      @click="$router.push('/')"
+      class="title"
+    >
+      Vue 3
+    </div>
+    <div class="navbar__btns">
+      <my-button @click="$router.push('/users')">Users</my-button>
+      <my-button @click="$router.push('/about')" style="margin-left: 20px;">About</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+.title {
+  font-size: 24px;
+  font-weight: 700;
+  cursor: pointer;
+}
+.navbar {
+  height: 50px;
+  background-color: rgb(216, 220, 220);
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+.navbar__btns {
+  margin-left: auto;
+}
+</style>
